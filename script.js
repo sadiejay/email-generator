4a2b62a1fa33935b9200dd1b4200546a63de1f10
sadiejay <19538219+sadiejay@users.noreply.github.com>
2022-02-10 21:32:48 -0600

    Updates game plan for js

--- script.js
+++ script.js
@@ -1,16 +1,16 @@
 // Goals
     // log input of first, last and domain name
-        // firstlast
-        // first.last
-        // first
-        // flast
     // remove any whitespace
     // change input to lower case
     // create different combos of first + last
-        // no regex(?) needed
-        // insert '.' between inputs
-        // only keep first name input
-        // use regex to match first character => add character to last name input
+        // firstlast 
+            // -- no regex(?) needed
+        // first.last 
+            // -- insert '.' between inputs
+        // first
+            // -- only keep first name input
+        // flast
+            // -- use regex to match first character => add character to last name input
     // append domain name to the end of the different combos
     // display all the different combos
     // copy option for each individual combo
