7d8c58ac5f7ed3075f199af0caf5662d4987fecb
sadiejay <19538219+sadiejay@users.noreply.github.com>
2022-02-10 18:45:31 -0600

    Adds breakdown of JS tasks

---
+++ script.js
@@ -0,0 +1,17 @@
+// Goals
+    // log input of first, last and domain name
+        // firstlast
+        // first.last
+        // first
+        // flast
+    // remove any whitespace
+    // create different combos of first + last
+        // no regex(?) needed
+        // insert '.' between inputs
+        // only keep first name input
+        // use regex to match first character => add character to last name input
+    // append domain name to the end of the different combos
+    // display all the different combos
+    // copy option for each individual combo
+
+console.log("hello")
